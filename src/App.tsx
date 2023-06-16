31a73f6b8087e7155e21c57e501a40c41ad391ae
muktadira22 <aryamuktadir22@gmail.com>
2023-06-16 13:47:41 +0700

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,67 @@
-import { 
-    Refine,
-    GitHubBanner, 
-    WelcomePage,
-    Authenticated, 
-} from '@refinedev/core';
-import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-
-    import { AuthPage,ErrorComponent
-,notificationProvider
-,RefineThemes
-,ThemedLayoutV2} from '@refinedev/chakra-ui';
-
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
-import routerBindings, { NavigateToResource, CatchAllNavigate, UnsavedChangesNotifier, DocumentTitleHandler } from "@refinedev/react-router-v6";
+import {
+  ErrorComponent,
+  ThemedLayoutV2,
+  notificationProvider,
+  RefineThemes,
+} from "@refinedev/chakra-ui";
+import { Refine } from "@refinedev/core";
+import routerBindings, {
+  NavigateToResource,
+  UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
 import dataProvider from "@refinedev/simple-rest";
-import { Header } from "./components/header";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { ChakraUIInferencer } from "@refinedev/inferencer/chakra-ui";
 
-
-
-
-
-function App() {
-    
-
-    
-    
-    return (
-        <BrowserRouter>
-        <GitHubBanner />
-        <RefineKbarProvider>
-            {/* You can change the theme colors here. example: theme={RefineThemes.Magenta} */}
-<ChakraProvider theme={RefineThemes.Blue}>
-            <Refine notificationProvider={notificationProvider}
-routerProvider={routerBindings}
-dataProvider={dataProvider("https://api.fake-rest.refine.dev")} 
-                options={{
-                    syncWithLocation: true,
-                    warnWhenUnsavedChanges: true,
-                     
-                }}
+const App = () => {
+  return (
+    <ChakraProvider theme={RefineThemes.Green}>
+      <BrowserRouter>
+        <Refine
+          notificationProvider={notificationProvider()}
+          routerProvider={routerBindings}
+          dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+          resources={[
+            {
+              name: "blog_posts",
+              list: "/blog-posts",
+              show: "/blog-posts/show/:id",
+              create: "/blog-posts/create",
+              edit: "/blog-posts/edit/:id",
+            },
+          ]}
+          options={{
+            syncWithLocation: true,
+            warnWhenUnsavedChanges: true,
+          }}
+        >
+          <Routes>
+            <Route
+              element={
+                <ThemedLayoutV2>
+                  <Outlet />
+                </ThemedLayoutV2>
+              }
             >
-
-
-                    <Routes>
-                        <Route index element={<WelcomePage />} />
-                    </Routes>
-                <RefineKbar />
-                <UnsavedChangesNotifier />
-                <DocumentTitleHandler />
-            </Refine>
-            </ChakraProvider>
-
-        </RefineKbarProvider>
-        </BrowserRouter>
-      );
+              <Route
+                index
+                element={<NavigateToResource resource="blog_posts" />}
+              />
+              <Route path="blog-posts">
+                <Route index element={<ChakraUIInferencer />} />
+                <Route path="show/:id" element={<ChakraUIInferencer />} />
+                <Route path="edit/:id" element={<ChakraUIInferencer />} />
+                <Route path="create" element={<ChakraUIInferencer />} />
+              </Route>
+              <Route path="*" element={<ErrorComponent />} />
+            </Route>
+          </Routes>
+          <UnsavedChangesNotifier />
+        </Refine>
+      </BrowserRouter>
+    </ChakraProvider>
+  );
 };
 
 export default App;
